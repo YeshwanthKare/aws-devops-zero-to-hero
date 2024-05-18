6583039b227d0fa0c7c218761ea6d7ce670d44f3
Yeshwanth Kare <64756016+YeshwanthKare@users.noreply.github.com>
2024-05-18 17:07:54 +0200

    Update start_container.sh new

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull docker pull yeshid/simple-python-flask-app:latest
+docker pull yeshid/simple-python-flask-app
 
 # Run the Docker image as a container
 docker run -d -p 5000:5000 yeshid/simple-python-flask-app
